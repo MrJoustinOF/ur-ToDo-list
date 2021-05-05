2897139d4bdb6ba942fdaa397c02eda56dcb5ce6
Joustin Ortiz <ortizjoustin@gmail.com>
2021-05-05 17:26:25 -0500

    Web complete

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,20 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { RouterModule, Route } from '@angular/router';
 
 import { AppComponent } from './app.component';
+import { AboutComponent } from './about/about.component';
+import { HomeComponent } from './home/home.component';
+
+const routes: Route[] = [
+  { path: '', component: HomeComponent },
+  { path: 'about', component: AboutComponent },
+];
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule
-  ],
+  declarations: [AppComponent, AboutComponent, HomeComponent],
+  imports: [BrowserModule, RouterModule.forRoot(routes)],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
