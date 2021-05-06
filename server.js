ece7a4a9aa2a577e500f9377ea130e18df0ead40
Joustin Ortiz <ortizjoustin@gmail.com>
2021-05-06 16:04:32 -0500

    new attempt to deploy

--- server.js
+++ server.js
@@ -1,8 +1,8 @@
 const express = require("express");
 const path = require("path");
 const app = express();
-app.use(express.static(__dirname + "/dist/ur-to-do-list"));
+app.use(express.static(__dirname + "/dist/ur-ToDo-list"));
 app.get("/*", function (req, res) {
-  res.sendFile(path.join(__dirname + "/dist/ur-to-do-list/index.html"));
+  res.sendFile(path.join(__dirname + "/dist/ur-ToDo-list/index.html"));
 });
 app.listen(process.env.PORT || 8080);
